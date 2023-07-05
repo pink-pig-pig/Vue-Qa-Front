e84400c0d54165ac09f287f8106de6f173540eac
zhangyazhong <zhangyazhong@kuaishou.com>
2023-07-05 20:44:04 +0800

    commit

---
+++ README.md
@@ -0,0 +1,19 @@
+你必须使用 Node 14.18+ / 16+ 版本。
+
+```bash
+# 克隆项目
+git clone https://github.com/pink-pig-pig/Vue-Qa-Front.git
+
+# 进入项目
+cd Vue-Qa-Front
+
+# 安装依赖
+cnpm install
+
+# 运行项目
+cnpm run dev
+
+# 打包发布
+cnpm run build
+```
+
---
+++ src/i18n/pages/login/zh-cn.ts
@@ -0,0 +1,28 @@
+// 定义内容
+export default {
+	label: {
+		one1: '用户名登录',
+		two2: '手机号登录',
+	},
+	link: {
+		one3: '第三方登录',
+		two4: '友情链接',
+	},
+	account: {
+		accountPlaceholder1: '用户名 admin 或不输均为 common',
+		accountPlaceholder2: '密码：123456',
+		accountPlaceholder3: '请输入验证码',
+		accountBtnText: '登 录',
+	},
+	mobile: {
+		placeholder1: '请输入手机号',
+		placeholder2: '请输入验证码',
+		codeText: '获取验证码',
+		btnText: '登 录',
+		msgText: '* 温馨提示：建议使用谷歌、Microsoft Edge',
+	},
+	scan: {
+		text: '打开手机扫一扫，快速登录/注册',
+	},
+	signInText: '欢迎回来！',
+};
